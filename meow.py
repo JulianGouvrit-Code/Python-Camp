1d1763195d8dcd4e966dbb21a25d0a1130f645f0
JulianGouvrit-Code <julian.gouvrit@gmail.com>
2026-07-21 18:10:36 -0400

    Update meow.py
    hi

--- meow.py
+++ meow.py
@@ -4,14 +4,21 @@ pygame.init()
 
 WIDTH = 500
 HEIGHT = 500
-screen = pygame.display.set_mode("WIDTH, HEIGHT")
+screen = pygame.display.set_mode(WIDTH, HEIGHT)
 
-clock = pygame.time.colock()
+clock = pygame.time.clock()
 
 pygame.display.set_caption("pong")
-
-player_pos = pygame.Vector2(screen.get_width()  /   2 screen.get_height() / 2 - paddle_height / 2)
-enemy.pos = pygame.Vector2(6  /screen.get_width() / 2 screen.get_height() / 2 - paddle_height / 2)
+paddle_width = 10
+paddle_height = 80
+player_pos = pygame.Vector2(
+    screen.get_width() - 20,
+    screen.get_height()/2 - paddle_height/2
+    enemy_pos = pygame.Vector2(
+    10,
+    screen.get_height()/2 - paddle_height/2
+)
+)
 FPS = 30
 
 running = True
@@ -19,3 +26,7 @@ running = True
 while running:
     for event in pygame.event.get():
         keys = pygame.event.get_pressed()
+
+for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+        running = False
